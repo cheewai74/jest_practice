f83ab2020cfcd2cc9689ce285d0f1b8e86cc4ed9
moses <academicsnotes@gmail.com>
2022-04-24 20:39:16 +0800

    Commit  test scripts

--- myJestPractice/multiply.test.js
+++ myJestPractice/multiply.test.js
@@ -5,3 +5,13 @@ describe('test multiply positive scenarios', ()=>{
         expect(multiply(3,2)).toBe(6);
     })
 })
+
+test('object example', () => {
+    const data = {first: 1};
+    data['second'] = 2;
+    expect(data).toEqual({first: 1, second: 2});
+});
+
+test('multiply 4*3 should not equal to 11', ()=>{
+    expect(multiply(4,3)).not.toBe(11);
+})
